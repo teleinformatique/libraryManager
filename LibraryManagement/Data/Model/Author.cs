be4993771b78fe8aad76e3aebaf4ac82c8645fa3
Khadim Sarr <khadimsarr@outlook.com>
2018-01-20 13:59:11 +0000

    Creation of models: customer, author, book

---
+++ LibraryManagement/Data/Model/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data.Model
+{
+    public class Author
+    {
+        public int AuthorID { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
---
+++ LibraryManagement/Data/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryManagement.Data.Model
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+
+        public virtual Author Author { get; set; }
+        public int AuthorID { get; set; }
+
+        public virtual Customer Borrower { get; set; }
+        public int CustomerID { get; set; }
+
+    }
+}
