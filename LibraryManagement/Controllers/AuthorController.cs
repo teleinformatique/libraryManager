2e287d5b151c044922011317eb47ef6c64b5b8f8
Khadim Sarr <khadimsarr@outlook.com>
2018-01-28 19:00:41 +0000

    Created Author repository

---
+++ LibraryManagement/Controllers/AuthorController.cs
@@ -0,0 +1,53 @@
+﻿using LibraryManagement.Data.Model;
+using LibraryManagement.Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Controllers
+{
+    public class AuthorController: Controller
+    {
+        private readonly AuthorRepository _authorRepository;
+
+        public AuthorController(AuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public IActionResult List()
+        {
+            var authors = _authorRepository.GetAll();
+
+            return View(authors);
+        }
+
+        public IActionResult Update(int id)
+        {
+            var author = _authorRepository.GetById(id);
+            if (author == null) return NotFound();
+
+            return View(author);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Author author)
+        {
+            _authorRepository.Update(author);
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var author = _authorRepository.GetById(id);
+            if (author == null) return NotFound();
+
+            _authorRepository.Delete(author);
+
+            return RedirectToAction("List");
+        }
+    }
+}
